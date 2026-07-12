947023d3ec8d9c56efe4e61316a4b07021d5595a
MD MAHFUZ HUSSEN MAHI <mdmahfuzhussen@gmail.com>
2026-07-12 16:21:18 +0600

    Update settings.py

--- dendjango/settings.py
+++ dendjango/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY = 'django-insecure-7!**@##oqyp+lt#u1_fe3$@v@)!(e30a84mxenjcgv)1g1bds!
 DEBUG = False
 
 # In settings.py
-ALLOWED_HOSTS = ['vercelexnuman.onrender.com', 'www.driversden.co.uk', 'driversden.co.uk']
-
+ALLOWED_HOSTS = [
+    "vercelexnuman.onrender.com",
+    "driversden.co.uk",
+    "www.driversden.co.uk",
+]
 # ==============================================================================
 # EMAIL CONFIGURATION (GMAIL SMTP)
 # ==============================================================================
